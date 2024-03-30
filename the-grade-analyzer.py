6fc3372d28cca55e5f084583bc4eb193ed891b93
balkanarchist <balkanarchist@yandex.com>
2024-03-29 21:23:34 -0700

    added function to task 1 and instructions for all in grade-analyzer

---
+++ the-grade-analyzer.py
@@ -0,0 +1,4 @@
+# Task 1: Code a function to calculate the average grade.
+def average_grade():
+# Task 2: Implement a function to find the highest and lowest grade.
+# Task 3: Create a feature that categorizes grades into letter grades (A, B, C, etc.).
