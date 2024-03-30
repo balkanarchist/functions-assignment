9fed4547e39edba307fd99230192d674bca10830
balkanarchist <balkanarchist@yandex.com>
2024-03-29 21:21:16 -0700

    added instructions and code for task 1 of quiz-game

---
+++ the-quiz-game.py
@@ -0,0 +1,8 @@
+# Task 1: Develop a list of questions and answers.
+questions = ['What is the capital of Okinawa?',
+             'What is the numerical value of the meaning of life?',
+             'What is the last name of the character in Shogun who is based on Tokugawa Ieyasu?',
+             'Boxers, briefs, or boxer briefs?']
+answers = ['Naha', '42', 'Toranaga', 'briefs']
+# Task 2: Write a function that quizzes the user and takes their answers.
+# Task 3: Score the quiz and give the user feedback on their performance.
