2cafa530b05863a791947a938a8ecad8bbf85500
balkanarchist <balkanarchist@yandex.com>
2024-03-29 21:27:21 -0700

    added instructions to calculator-app

---
+++ the-calculator-app.py
@@ -0,0 +1,3 @@
+# Task 1: Create functions for each (basic) arithmetic operation.
+# Task 2: Implement user input to receive numbers and an operation choice.
+# Task 3: Ensure your program can handle division by zero and other potential errors.
