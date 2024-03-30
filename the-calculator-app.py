5dbbc4c4f569147aba9643a7b238037246917c4a
balkanarchist <balkanarchist@yandex.com>
2024-03-29 21:31:11 -0700

    added code to Task 1 of calculator app

--- the-calculator-app.py
+++ the-calculator-app.py
@@ -1,3 +1,18 @@
 # Task 1: Create functions for each (basic) arithmetic operation.
+def addition(num1, num2):
+    return num1 + num2
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "ERROR: Cannot divide by zero."
+    
 # Task 2: Implement user input to receive numbers and an operation choice.
 # Task 3: Ensure your program can handle division by zero and other potential errors.
